b20b498f8df7a6101008fe04a0ad0b78d60e6035
WeshallPersistent <vishal_nimase@persistent.com>
2023-06-28 15:56:48 +0530

    initial page added

---
+++ frontend/src/Component/Home.js
@@ -0,0 +1,13 @@
+
+export const Home = () => {
+  return (
+    <>
+      <div className="mt-64">
+         <input className="relative w-4/12 rounded-lg px-8 py-4 text-sm border-none shadow-lg shadow-black" type='text' placeholder='Please add Api Name'/> 
+         <button>
+          Create API
+         </button>
+      </div>
+    </>
+  )
+}
