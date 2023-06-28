48ebeed79ea63857d2a2843c34e67b5b8df1084a
Abhinav Kumar <abhinav_kumar2@persistent.com>
2023-06-28 16:52:51 +0530

    Added home page with input box and create button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { Home } from './Component/Home'
+import "./App.css";
+import Home from "./Component/Home";
 
 function App() {
   return (
     <div className="App bg-gray-400">
-      <header className="text-4xl font-bold text-red-500 text-center flex flex-col items-center ">
+      <header className="text-4xl font-bold text-red-500 text-center flex flex-col items-center pt-16">
         API Monitoring
       </header>
       <Home />
--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -1,13 +1,42 @@
+import React, { useState } from "react";
 
-export const Home = () => {
+const Home = () => {
+  const [inputName, setInputName] = useState("");
+  const [apiName, setApiName] = useState("");
+
+  const onInputAPIChange = (e) => {
+    setInputName(e.target.value);
+  };
+
+  const onSubmitAPIName = () => {
+    setApiName(inputName);
+  };
+
+  console.log(apiName);
+
   return (
     <>
-      <div className="mt-64">
-         <input className="relative w-4/12 rounded-lg px-8 py-4 text-sm border-none shadow-lg shadow-black" type='text' placeholder='Please add Api Name'/> 
-         <button>
+      <div className="mt-48 ">
+        <input
+          value={inputName}
+          onChange={(e) => {
+            onInputAPIChange(e);
+          }}
+          className="w-4/12 rounded-lg px-8 py-4 text-sm border-none shadow-lg shadow-black"
+          type="text"
+          placeholder="Please Add API Name"
+        />
+        <button
+          className="ml-4 w-24 h-12 rounded-lg bg-sky-700 text-white text-sm"
+          onClick={() => {
+            onSubmitAPIName();
+          }}
+        >
           Create API
-         </button>
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
+
+export default Home;
