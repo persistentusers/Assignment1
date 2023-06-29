acd7ccddd2ed4f9388e5c5e9170442ccc86cee00
Abhinav Kumar <abhinav_kumar2@persistent.com>
2023-06-29 17:50:49 +0530

    changes in api details

--- frontend/src/Component/APIDetails.js
+++ frontend/src/Component/APIDetails.js
@@ -3,9 +3,9 @@ import React from "react";
 const APIDetails = () => {
   return (
     <div className="p-4 mt-4 bg-white shadow-lg h-96 shadow-black">
-      <div className="float-left ">
+      <div className="float-left w-1/3 h-96">
         <button
-          className="tablinks"
+          className="block w-auto font-bold transition tablinks bg-slate-50 text-emerald-200"
           onclick="openCity(event, 'London')"
           id="defaultOpen"
         >
@@ -13,8 +13,8 @@ const APIDetails = () => {
         </button>
       </div>
 
-      <div id="London" className="tabcontent">
-        <h3>API URL</h3>
+      <div id="apiUrl" className="tabcontent">
+        <h3 className="font-bold">API URL - agsdjasgdjkagsdkjg</h3>
         <p>Request </p>
         <p>Response </p>
       </div>
