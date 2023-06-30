85c578cb7473066d13dccafff94942156251bd7f
Abhinav Kumar <abhinav_kumar2@persistent.com>
2023-06-30 17:45:50 +0530

    added details tab in API details

--- frontend/src/Component/APIDetails.js
+++ frontend/src/Component/APIDetails.js
@@ -1,22 +1,105 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const APIDetails = () => {
+  const [activeTab, setActiveTab] = useState({ name: "", url: "" });
+  const [detailsTab, setDetailsTab] = useState("");
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    setActiveTab({
+      ...activeTab,
+      name: apiData[0]?.apiName,
+      url: apiData[0]?.apiUrl,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const apiData = [{ apiName: "apiName1 ", apiUrl: "apiUrl1" }];
+
+  const onActiveTabChange = (tabInfo) => {
+    setActiveTab({
+      ...activeTab,
+      name: tabInfo?.apiName,
+      url: tabInfo?.apiUrl,
+    });
+  };
+
+  const onActiveDetails = (activeDetail) => {
+    setDetailsTab(activeDetail);
+    axios({
+      method: "get",
+      url: "https://rickandmortyapi.com/api/character/2",
+    }).then((response) => {
+      console.log(response);
+      const res = response?.data;
+      const req = response?.request;
+
+      if (activeDetail === "response") {
+        setDetails({
+          ...details,
+          data: res,
+        });
+      }
+      if (activeDetail === "request") {
+        setDetails({
+          ...details,
+          data: req,
+        });
+      }
+    });
+  };
+  console.log(activeTab, "activeTab", details);
+
   return (
-    <div className="p-4 mt-4 bg-white shadow-lg h-96 shadow-black">
-      <div className="float-left w-1/3 h-96">
-        <button
-          className="block w-auto font-bold transition tablinks bg-slate-50 text-emerald-200"
-          onclick="openCity(event, 'London')"
-          id="defaultOpen"
-        >
-          API Name
-        </button>
+    <div className="mt-4 bg-white shadow-lg h-96 shadow-black">
+      <div className="float-left border border-black border-solid h-full bg-[#ccc] w-48">
+        {apiData?.map((info) => {
+          return (
+            <div
+              onClick={() => {
+                onActiveTabChange(info);
+              }}
+              className={`block p-4 font-bold text-black transition  ${
+                activeTab?.name === info?.apiName ? "bg-[#aaada9]" : "bg-none"
+              }`}
+              id={info}
+            >
+              {info.apiName}
+            </div>
+          );
+        })}
       </div>
 
-      <div id="apiUrl" className="tabcontent">
-        <h3 className="font-bold">API URL - agsdjasgdjkagsdkjg</h3>
-        <p>Request </p>
-        <p>Response </p>
+      <div id={activeTab?.name} className="pt-4">
+        <h3 className="font-bold">API URL - {activeTab?.url}</h3>
+        <div className="flex gap-2 pl-4">
+          <div
+            className={`block w-auto cursor-pointer ${
+              detailsTab === "request" ? "text-blue-500" : ""
+            }`}
+            onClick={() => {
+              onActiveDetails("request");
+            }}
+          >
+            Request{" "}
+          </div>
+          <div
+            className={`block w-auto cursor-pointer ${
+              detailsTab === "response" ? "text-blue-500" : ""
+            }`}
+            onClick={() => {
+              onActiveDetails("response");
+            }}
+          >
+            Response{" "}
+          </div>
+          {/* <div>
+            <div className="pt-16">
+              <pre>{JSON.stringify(details?.data, null, 1)}</pre>
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   );
