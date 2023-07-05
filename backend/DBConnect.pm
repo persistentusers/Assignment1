c88521ae66784fc973b21f5be1acf04d7e26dcd5
shivendu_parashar <shivendu_parashar@persistent.com>
2023-07-05 11:31:40 +0530

    resolved the 1st PR comments against backend

--- backend/DBConnect.pm
+++ backend/DBConnect.pm
@@ -3,6 +3,7 @@ package DBConnect;
 use strict;
 use warnings;
 use DBI;  
+use JSON;
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -20,27 +21,33 @@ sub connect{
 }
 
 sub insert_api_name{
-    my $sth = $dbh->prepare("INSERT INTO ADDAPI
-                            (API_NAME)
-                            values
-                            (?)");
+    my $sth = $dbh->prepare("INSERT INTO ADDAPI (API_NAME) values (?)");
     my $rv=$sth->execute(@_) 
             or die $DBI::errstr;
-    if(! defined $rv) {print "OOPs! row cannot be inserted.\n";}
-    else {print "Row added successfully.\n";}
+    if(! defined $rv) {return 0;}
+    else {return 1;}
     $sth->finish();
 }
 
 sub fetch_api_name{
-    my $sth = $dbh->prepare("SELECT * FROM ADDAPI");  
+    my $sth = $dbh->prepare("SELECT API_ID,API_NAME FROM ADDAPI");  
     $sth->execute() or die $DBI::errstr;  
+
+    my @api_names;
+    
     while (my @row = $sth->fetchrow_array()) {  
        my ($API_ID,$API_NAME) = @row;  
-        print "API_ID = $API_ID, API_NAME = $API_NAME\n";
-    # return $API_ID,$API_NAME;  
+
+        push @api_names, {
+            API_ID => $API_ID,
+            API_NAME => $API_NAME
+        };
     }  
     $sth->finish();
+        my $json_response = JSON->new->encode(\@api_names);
+        return $json_response; 
 }
+
 
 sub insert_api_url{
     my ($API_NAME, $API_URL) = @_;
@@ -52,55 +59,52 @@ sub insert_api_url{
     $sth_select->finish();
 
     #Insert API_ID and API_URL into ADDAPI_URL table.
-    my $sth_insert = $dbh->prepare("INSERT INTO ADDAPI_URL
-                            (API_ID,API_URL)
-                            values
-                            (?,?)");
+    my $sth_insert = $dbh->prepare("INSERT INTO ADDAPI_URL (API_ID,API_URL) values (?,?)");
     my $rv=$sth_insert->execute($API_ID,$API_URL) or die $dbh->errstr; 
-            # or die $DBI::errstr;
-    if(! defined $rv) {print "OOPs! row cannot be inserted.\n";}
-    else {print "Row added successfully.\n";}
+    if(! defined $rv) {return 0;}
+    else {return 1;}
     $sth_insert->finish();
 }
 
 sub fetch_api_url{
-    my $sth = $dbh->prepare("SELECT * FROM ADDAPI_URL");  
+    my $sth = $dbh->prepare("SELECT URL_ID,API_ID,API_URL FROM ADDAPI_URL");  
     $sth->execute() or die $DBI::errstr;  
+    my @api_url;
     while (my @row = $sth->fetchrow_array()) {  
-       my ($URL_ID,$API_ID,$API_URL) = @row;  
-        print "URL_ID = $URL_ID, API_ID = $API_ID, API_URL = $API_URL\n";  
-   # return $URL_ID,$API_ID,$API_URL;
+       my ($URL_ID,$API_ID,$API_URL) = @row;   
+       push @api_url, {
+            URL_ID => $URL_ID,
+            API_ID => $API_ID,
+            API_URL => $API_URL
+        };
     }  
     $sth->finish();
+    my $json_response = JSON->new->encode(\@api_url);
+    return $json_response;
 }
 
 sub insert_api_status{
-    my $sth = $dbh->prepare("INSERT INTO ADDAPI_STATUS
-                            (URL_ID,URL_STATUS,RESPONSE_TIME)
-                            values
-                            ( ?,?,?)");
+    my $sth = $dbh->prepare("INSERT INTO ADDAPI_STATUS (URL_ID,URL_STATUS,RESPONSE_TIME) values ( ?,?,?)");
     my $rv=$sth->execute(@_) 
             or die $DBI::errstr;
-    if(! defined $rv) {print "OOPs! row cannot be inserted.\n";}
-    else {print "Row added successfully.\n";}
+    if(! defined $rv) {return 0;}
+    else {return 1;}
     $sth->finish();
 }
 
 sub fetch_api_status{
     my ($API_NAME) = @_;
-
+    
     #Fetch API_ID based on API_NAME
     my $sth_select = $dbh->prepare("SELECT API_ID FROM ADDAPI WHERE API_NAME = ?");  
     $sth_select->execute($API_NAME) or die $dbh->errstr; 
     my ($API_ID) = $sth_select->fetchrow_array;
-    # print "API_ID=$API_ID\n";
     $sth_select->finish();
 
     #Fetch URL_ID based on API_ID
     my $sth_select_urlid = $dbh->prepare("SELECT MAX(URL_ID) FROM ADDAPI_URL WHERE API_ID = ?") ;
     $sth_select_urlid->execute($API_ID) or die $dbh->errstr; 
     my ($URL_ID) = $sth_select_urlid->fetchrow_array;
-    # print "URL_ID=$URL_ID\n";
     $sth_select_urlid->finish();
 
     #Fetch RESPONSE_TIME based on URL_ID
@@ -122,13 +126,16 @@ sub fetch_api_status{
     $sth_select_fail->finish();
 
     my $total_count = $countp + $countf;
- 
-    print "Total Count = $total_count\n";    
-    print "Count Pass = $countp\n";
-    print "Count Fail = $countf\n";
-    print "RESPONSE_TIME = $RESPONSE_TIME\n";
-    
-     #return ($total_count,$countp,$countf,$RESPONSE_TIME );
-   
+
+    my %api_status = (
+            'Total Count' => $total_count,
+            'Count Pass' => $countp,
+            'Count Fail' => $countf,
+            'RESPONSE_TIME' => $RESPONSE_TIME
+    );
+
+    my $json = encode_json \%api_status;
+    return $json;
 }
+
 1;
